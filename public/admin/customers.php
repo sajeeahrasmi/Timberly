9a529741e7c57f84322057c11422b912cafd5df6
Pawan Weerasuriya <uwpawan@gmail.com>
2024-11-25 12:44:55 +0530

    [Pawan] customers file edited. UI done!

--- public/admin/customers.php
+++ public/admin/customers.php
@@ -18,41 +18,44 @@ $customerData = [
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="./styles/customers.css">
+        <link rel="stylesheet" href="./styles/components/header.css">
+        <link rel="stylesheet" href="./styles/components/sidebar.css">
     </head>
     <body>
-        <div class="page-content">
-            <div class="main-content">
-                <div class="orders-display-box">
-                    <div style="display: flex;
-                                justify-content: space-between;
-                                align-items: center;"
-                         class="content-header">
-                        <h2>Customers</h2>
-                        <a href="./addCustomer.php"><i class="fa-solid fa-circle-plus" style="margin-right: 8px"></i>Add Customer</a>
-                    </div>
-                    <table class="product-table">
-                        <thead>
-                            <tr>
-                                <th>Customer ID</th>
-                                <th>Name</th>
-                                <th>Registered on</th>
-                                <th>Telephone no</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($customerData as $item): ?>
-                            <tr>
-                                <td><?php echo $item['cust_id']; ?></td>
-                                <td><?php echo $item['name']; ?></td>
-                                <td><?php echo $item['registered_on']; ?></td>
-                                <td><?php echo $item['tele_num']; ?></td>
-                                <td><?php echo $item['email']; ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
+    <div class="dashboard-container">
+        <?php include "./components/sidebar.php" ?>
+        <div class="main-content">
+            <?php include "./components/header.php" ?>
+            <div class="orders-display-box">
+                <div style="display: flex;
+                            justify-content: space-between;
+                            align-items: center;"
+                        class="content-header">
+                    <h2>Customers</h2>
+                    <a href="./addCustomer.php"><i class="fa-solid fa-circle-plus" style="margin-right: 8px"></i>Add Customer</a>
                 </div>
+                <table class="product-table">
+                    <thead>
+                        <tr>
+                            <th>Customer ID</th>
+                            <th>Name</th>
+                            <th>Registered on</th>
+                            <th>Telephone no</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($customerData as $item): ?>
+                        <tr>
+                            <td><?php echo $item['cust_id']; ?></td>
+                            <td><?php echo $item['name']; ?></td>
+                            <td><?php echo $item['registered_on']; ?></td>
+                            <td><?php echo $item['tele_num']; ?></td>
+                            <td><?php echo $item['email']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </body>
