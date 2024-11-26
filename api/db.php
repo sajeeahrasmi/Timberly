cc3e9897e8d71436c4a8fdef2be227e9e378f437
Pawan Weerasuriya <uwpawan@gmail.com>
2024-11-26 18:02:50 +0530

    [Pawan] dbname=timberly

--- api/db.php
+++ api/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; 
 $password = ""; 
-$dbname = "timberlyy";
+$dbname = "timberly";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
