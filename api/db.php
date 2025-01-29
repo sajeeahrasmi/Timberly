c30e3bd35d2e9a9ac62da1605eba8a0d11fe3587
sajeeahrasmi <sajeeahrasmi@gmail.com>
2025-01-29 14:43:17 +0530

    Database solved

--- api/db.php
+++ api/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root"; 
-$password = "new_password"; 
-$dbname = "newtimberly";
+$password = ""; 
+$dbname = "timberly";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
