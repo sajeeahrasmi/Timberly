bccdb669974475344ebf07b1ae24ca69a27a41ff
Rishi20020825 <rasheenmohommed25@gmail.com>
2025-02-23 17:52:59 +0530

    Order flow for lumber 90 percent Completed{Rishi - Manger}

---
+++ api/updateStatus.php
@@ -0,0 +1,38 @@
+<?php
+// Include the database connection
+require_once 'db.php';
+
+// Get the POST data
+$orderId = isset($_POST['orderId']) ? $_POST['orderId'] : '';
+$itemId = isset($_POST['itemId']) ? $_POST['itemId'] : '';
+$status = isset($_POST['status']) ? $_POST['status'] : '';
+
+// Update the order status in the database
+if ($status== 'Confirmed'){
+    $sql = "UPDATE orders 
+        SET status = ? 
+        WHERE orderId = ? ";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("si", $status, $orderId);
+}
+
+else{
+    $sql = "UPDATE orderlumber
+    SET status = ?
+    WHERE orderId = ? AND itemId = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sii", $status, $orderId, $itemId);
+}
+
+
+if ($stmt->execute()) {
+    // Respond back with success
+    echo json_encode(["status" => "success", "message" => "Order status updated successfully"]);
+} else {
+    // Respond back with an error
+    echo json_encode(["status" => "error", "message" => "Failed to update order status"]);
+}
+
+$stmt->close();
+$conn->close();
+?>
