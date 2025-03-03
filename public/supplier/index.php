ed12c25de3a6e6bc7328232f7912bafc0bde649a
Theekshana Thathsara <theekshanathathsara.2001@gmail.com>
2025-03-03 19:11:30 +0530

    create layout index file

---
+++ public/supplier/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/Supplier/styles/index.css"> <!-- Link to your CSS file -->
+</head>
+<body>
+
+<div class="header-content">
+    <?php include 'components/header.php'; ?>
+</div>
+
+<!-- Wrap Sidebar and Body in .body-container -->
+<div class="body-container">
+    <!-- Sidebar -->
+    <div class="sidebar-content">
+        <?php include 'components/sidebar.php'; ?>
+    </div>
+
+    <!-- Main Content Area -->
+    <div class="body-content">
+        <h1>Layout Section</h1>
+        <p>This is the main content area where you can write.</p>
+    </div>
+</div>
+
+</body>
+</html>
