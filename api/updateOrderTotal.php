5f55c7cfeaaf274a815d3b288494fba8e7f159e1
Rishi20020825 <rasheenmohommed25@gmail.com>
2025-04-15 18:33:22 +0530

    Bug Fixed{manager}

--- api/updateOrderTotal.php
+++ api/updateOrderTotal.php
@@ -2,6 +2,7 @@
 require_once 'db.php';
 
 $orderId = $_POST['orderId'];
+$dfree = $_POST['dfree'] ?? 0; // Default to 0 if not set
 
 $sql = "SELECT ol.qty, l.unitPrice 
         FROM orderlumber ol 
@@ -17,7 +18,7 @@ while ($row = $result->fetch_assoc()) {
     $totalAmount += $row['qty'] * $row['unitPrice'];
 }
 $stmt->close();
-
+$totalAmount += $dfree; // Add the delivery fee if applicable
 $sql = "UPDATE orders SET totalAmount = ? WHERE orderId = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("di", $totalAmount, $orderId);
