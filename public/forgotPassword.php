d33218b33c1efb33a8d3911d5e0def15c8abc4a7
Pawan Weerasuriya <uwpawan@gmail.com>
2025-04-24 00:50:43 +0530

    [Pawan] login page edited much lovely.

--- public/forgotPassword.php
+++ public/forgotPassword.php
@@ -7,10 +7,10 @@
 </head>
 <body>
     <div class="container">
-        <form action="../config/sendResetLink.php" method="POST">
-            <h2>Reset Password</h2>
+        <form action="../config/sendResetLink.php" method="POST" style="padding: 20px">
+            <h2 style="text-align: left">Reset Password</h2>
             <div class="form-group">
-                <label for="email">Enter your registered email</label>
+                <label style="font-weight: 300; color: #B18068" for="email">Enter your registered email</label>
                 <input type="email" name="email" id="email" required>
             </div>
             <button type="submit" class="button solid">Send Reset Link</button>
