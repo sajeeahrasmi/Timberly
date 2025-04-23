e938d7a6ef53c4f0aea8fec2a41f0c1f15b64f2a
Pawan Weerasuriya <uwpawan@gmail.com>
2025-04-23 19:30:03 +0530

    [Pawan] paginations applied to postProducts.php

--- api/getPostProducts.php
+++ api/getPostProducts.php
@@ -1,6 +1,18 @@
 <?php
 include 'db.php';
 
+// Pagination setup
+$ordersPerPage = 7;
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
+$offset = ($page - 1) * $ordersPerPage;
+
+// Get total number of orders
+$totalOrdersQuery = "SELECT COUNT(*) as total FROM furnitures";
+$totalOrdersResult = mysqli_query($conn, $totalOrdersQuery);
+$totalOrdersRow = mysqli_fetch_assoc($totalOrdersResult);
+$totalOrders = $totalOrdersRow['total'];
+$totalPages = ceil($totalOrders / $ordersPerPage);
+
 // Query to fetch driver details
 $productDataQuery = "SELECT
                     furnitureId,
@@ -9,7 +21,8 @@ $productDataQuery = "SELECT
                     type,
                     size,
                     unitPrice
-                    from furnitures";
+                    from furnitures
+                    LIMIT $ordersPerPage OFFSET $offset";
 $productDataResult = mysqli_query($conn, $productDataQuery);
 
 if (!$productDataResult) {
