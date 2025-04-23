28c13670cfba30af1ea9d7ec114662e154ad2b95
Pawan Weerasuriya <uwpawan@gmail.com>
2025-04-23 20:05:49 +0530

    [Pawan] paginations applied to postRawLumber.php

--- api/getPostRawLumber.php
+++ api/getPostRawLumber.php
@@ -1,8 +1,20 @@
 <?php
     include 'db.php';
 
+    // Pagination setup
+    $ordersPerPage = 6;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
+    $offset = ($page - 1) * $ordersPerPage;
+
+    // Get total number of orders
+    $totalOrdersQuery = "SELECT COUNT(*) as total FROM lumber";
+    $totalOrdersResult = mysqli_query($conn, $totalOrdersQuery);
+    $totalOrdersRow = mysqli_fetch_assoc($totalOrdersResult);
+    $totalOrders = $totalOrdersRow['total'];
+    $totalPages = ceil($totalOrders / $ordersPerPage);
+
     // Query to fetch driver details
-    $productDataQuery = "SELECT * from lumber";
+    $productDataQuery = "SELECT * from lumber LIMIT $ordersPerPage OFFSET $offset";
     $productDataResult = mysqli_query($conn, $productDataQuery);
 
     if (!$productDataResult) {
