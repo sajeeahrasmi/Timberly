2ab39a4f536c4c5f690194400560d807d7e6fec1
Theekshana Thathsara <theekshanathathsara.2001@gmail.com>
2025-04-26 12:52:47 +0530

    minor updates and bug file fixes

--- public/supplier/index.php
+++ public/supplier/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="/Supplier/styles/index.css"> <!-- Link to your CSS file -->
+    <link rel="stylesheet" href="/Supplier/styles/index.css"> 
 </head>
 <body>
 
@@ -9,7 +9,6 @@
     <?php include 'components/header.php'; ?>
 </div>
 
-<!-- Wrap Sidebar and Body in .body-container -->
 <div class="body-container">
     <!-- Sidebar -->
     <div class="sidebar-content">
@@ -19,7 +18,7 @@
     <!-- Main Content Area -->
     <div class="body-content">
         <h1>Layout Section</h1>
-        <p>This is the main content area where you can write.</p>
+        <p>Main content area.</p>
     </div>
 </div>
 
